07c11d7fe43ac53a5e1cd9f8c59eb3b7b84d8748
steph-rage <stephlage621@gmail.com>
2016-12-20 14:54:23 -0500

    Now actually able to grab question text from POST

--- server_input_example.py
+++ server_input_example.py
@@ -1,5 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib import parse
 
+questions = []
 
 class Handler(BaseHTTPRequestHandler):
 	def do_GET(self):
@@ -14,8 +16,11 @@ class Handler(BaseHTTPRequestHandler):
 		self.end_headers()
 		with open('input_questions.html', 'r') as html_file:
 			self.wfile.write(bytes(html_file.read(), 'utf8'))
-		#Get the question sent via POST
-		question = self.rfile.read(int(self.headers.get('content-length')))
+		#Get the question sent via POST:
+		#get the correct length of the question from the headers, then read in from rfile
+		#decode into utf8, parse using urllib, then split for just the question text
+		new_question = parse.unquote_plus(self.rfile.read(int(self.headers.get('content-length'))).decode('utf8')).split('=')
+		print(new_question[1])
 		return
 
 
