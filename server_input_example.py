e67cafdbd9a6a50f1d1bdcb29c47b056668905cd
steph-rage <stephlage621@gmail.com>
2016-12-20 15:06:35 -0500

    Can input questions and print to page! Yay!

--- server_input_example.py
+++ server_input_example.py
@@ -2,26 +2,37 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib import parse
 
 questions = []
+html_footer = "</body></html>"
 
 class Handler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		self.send_response(200)
 		self.end_headers()
 		with open('input_questions.html', 'r') as html_file:
-			self.wfile.write(bytes(html_file.read(), 'utf8'))
+			html = html_file.read() 
+			html += html_footer
+			self.wfile.write(bytes(html, 'utf8'))
 		return
 
 	def do_POST(self):
 		self.send_response(200)
 		self.end_headers()
-		with open('input_questions.html', 'r') as html_file:
-			self.wfile.write(bytes(html_file.read(), 'utf8'))
 		#Get the question sent via POST:
 		#get the correct length of the question from the headers, then read in from rfile
 		#decode into utf8, parse using urllib, then split for just the question text
 		new_question = parse.unquote_plus(self.rfile.read(int(self.headers.get('content-length'))).decode('utf8')).split('=')
-		print(new_question[1])
+		questions.append(new_question[1])
+		with open('input_questions.html', 'r') as html_file:
+			html = html_file.read()
+			html += "<ol>"
+			for question in questions:
+				html += "<li>{}</li>".format(question)
+			html += "</ol>"
+			html += html_footer
+			self.wfile.write(bytes(html, 'utf8'))
 		return
+
+
 
 
 def run(server_class=HTTPServer, handler_class=Handler):
